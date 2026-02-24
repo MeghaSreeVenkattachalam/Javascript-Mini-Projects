7f5c252ca442b2febb96ab50b9f99904bf974a89
MEGHA SREE VENKATTACHALAM <meghavenkattachalam@gmail.com>
2026-02-24 12:31:20 +0530

    Add color picker functionality in script.js

---
+++ Color Picker/script.js
@@ -0,0 +1,21 @@
+let colorPickerContainerElement = document.getElementById("colorPickerContainer");
+
+function onClicking1() {
+    colorPickerContainerElement.style.backgroundColor = "#e0e0e0";
+    document.getElementById("selectedColorHexCode").textContent = "#e0e0e0";
+}
+
+function onClicking2() {
+    colorPickerContainerElement.style.backgroundColor = "#6fcf97";
+    document.getElementById("selectedColorHexCode").textContent = "#6fcf97";
+}
+
+function onClicking3() {
+    colorPickerContainerElement.style.backgroundColor = "#56ccf2";
+    document.getElementById("selectedColorHexCode").textContent = "#56ccf2";
+}
+
+function onClicking4() {
+    colorPickerContainerElement.style.backgroundColor = "#bb6bd9";
+    document.getElementById("selectedColorHexCode").textContent = "#bb6bd9";
+}
