308ceb5d976531311673911fbbcb45092724173e
MEGHA SREE VENKATTACHALAM <meghavenkattachalam@gmail.com>
2026-02-24 14:11:55 +0530

    Create script.js

---
+++ Seasons Switcher/script.js
@@ -0,0 +1,27 @@
+function Spring() {
+    document.getElementById("seasonSmallImage").src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png";
+    document.body.backgroundSize = "cover";
+    document.getElementById("seasonMediumImage").src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png"
+    document.body.backgroundSize = "cover";
+}
+
+function Summer() {
+    document.getElementById("seasonSmallImage").src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png";
+    document.body.backgroundSize = "cover";
+    document.getElementById("seasonMediumImage").src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-md-img.png";
+    document.body.backgroundSize = "cover";
+}
+
+function Autumn() {
+    document.getElementById("seasonSmallImage").src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-xs-img.png";
+    document.body.backgroundSize = "cover";
+    document.getElementById("seasonMediumImage").src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png";
+    document.body.backgroundSize = "cover";
+}
+
+function Winter() {
+    document.getElementById("seasonSmallImage").src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png";
+    document.body.backgroundSize = "cover";
+    document.getElementById("seasonMediumImage").src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-md-img.png";
+    document.body.backgroundSize = "cover";
+}
